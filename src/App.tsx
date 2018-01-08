2ac79ae370475296a3511cd9cf629ad70ecfa07c
Shahryar Tariq <st876@cornell.edu>
2018-01-08 19:05:57 +0500

    yo whaddup

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,67 @@
 import * as React from 'react';
-import {Stage, Layer, Rect} from 'react-konva'
+import {Stage, Layer, Circle} from 'react-konva'
 import './App.css';
 
-class App extends React.Component {
+
+
+
+
+
+
+class Graph extends React.Component {
   state = {
-    color: 'blue'
+    graphMatrix: [[]],
+    marker: [,],
   }
+  render() {
+    return(
+
+      
+     
+    );
+  }
+  handleClick() {
+
+
+
+  }
+}
+
+class Node extends React.Component {
+  state = {
+    cleaned: false,
+  }
+  render() {
+    return(
+      <Circle
+      x={50}
+      y={50}
+      radius={20}
+      fill={this.state.cleaned ? 'red':'blue'}
+      shadowBlur={5}
+      shadowEnabled={true}
+      />
+    );
+  }
+}
+
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Stage width={window.innerWidth} height={window.innerHeight} backgroundcolor={0x1099bb}>
           <Layer>
-            <Rect
-              x={50}
-              y={50}
-              width={50}
-              height={50}
-              fill={this.state.color}
-              shadowBlur={10}
-              onClick={this.handleClick}
-              />
+            <Graph/>
             </Layer>
         </Stage>
         
       </div>
     );
+  }
+  handleClick() {
+
+
   }
 }
 
