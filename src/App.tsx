06d7f511733bcdda2e944c38e9dd01711aaba816
Shahryar Tariq <st876@cornell.edu>
2018-01-09 18:05:16 +0500

    color change

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,87 @@
 import * as React from 'react';
-import {Stage, Layer, Circle} from 'react-konva'
+import { Sigma, RandomizeNodePositions, RelativeSize } from 'react-sigma';
 import './App.css';
 
-
-
-
-
-
+type Node = {
+  id: string,
+  color: string,
+};
 
 class Graph extends React.Component {
   state = {
-    graphMatrix: [[]],
-    marker: [,],
+
+    myGraph : {nodes: [
+        {id: 'n1', color: '#ff0000'}, 
+        {id: 'n2', color: '#ff0000'}], 
+        edges: [{id: 'e1', source: 'n1', target: 'n2'}]},
+    marker: '',
+
+  };
+  constructor(props: any) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
   render() {
-    return(
+    return (
 
+        <Sigma 
+          style={{width: '800px', height: '800px'}} 
+          graph={this.state.myGraph} 
+          settings={{drawEdges: true, clone: false,
+                     edgeColor: 'default'}}
+          onClickNode={this.handleClick}
+        >
+          <RelativeSize initialSize={15}/>
+          <RandomizeNodePositions/>
+        </Sigma>
+    );
+  }
+  handleClick(e: any) {
+
+    let edgeNodes = this.state.myGraph.edges;
+    let graphNodes = this.changeColor(e.data.node.id);
+    let markerChange = this.setMarker(e.data.node.id);
+    this.setState({myGraph: {nodes: graphNodes, edges: edgeNodes}, marker: markerChange});
+    console.log(this.state.myGraph);
+
+  }
+
+  changeColor(id: string): Array<Node> {
+    let graphNodes = this.state.myGraph.nodes.slice();
+    graphNodes.forEach((value) => {
+      if (value.id === id) {
+        value.color = '#0000ff';
+      }
+    });
+    return graphNodes;
+  }
+
+  setMarker(id: string): string {
+    let currMarker = this.state.marker;
+    if (currMarker === '') {
+      currMarker = id;
       
-     
-    );
-  }
-  handleClick() {
-
-
-
-  }
-}
-
-class Node extends React.Component {
-  state = {
-    cleaned: false,
-  }
-  render() {
-    return(
-      <Circle
-      x={50}
-      y={50}
-      radius={20}
-      fill={this.state.cleaned ? 'red':'blue'}
-      shadowBlur={5}
-      shadowEnabled={true}
-      />
-    );
+    } else {
+      this.state.myGraph.edges.forEach((value) => {
+        if (value.source === currMarker && value.target === id) {
+          currMarker = value.target;
+        } else if (value.source === id && value.target === currMarker) {
+          currMarker = value.source;
+        }
+      });
+    }
+    return currMarker;
   }
 }
-
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Stage width={window.innerWidth} height={window.innerHeight} backgroundcolor={0x1099bb}>
-          <Layer>
-            <Graph/>
-            </Layer>
-        </Stage>
-        
+        <Graph />
       </div>
     );
   }
-  handleClick() {
-
-
-  }
+  
 }
-
-
 export default App;
