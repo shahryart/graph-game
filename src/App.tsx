f79a68ed95ca88760d05768c9bdfc7ec34388f67
Shahryar Tariq <st876@cornell.edu>
2018-01-11 20:16:55 +0500

    change options

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import './App.css';
 import { Graph } from './Graph/Graph';
+import Dropdown from 'react-dropdown';
+
+const options = [ 'default-triangle'];
 
 class App extends React.Component {
   render() {
@@ -8,6 +11,13 @@ class App extends React.Component {
       <div className="App">
         <div className="App-header">
           <h2>nice</h2>
+          <Dropdown 
+            options={options} 
+            onChange={this.onSelect}
+            placeholder="Select an option" 
+          />
+          <input ref="input" defaultValue="1"/>
+          <button type="change" className="btn btn-primary">Change</button>
         </div>
         <p className="Planar Graph Game">
         <Graph
@@ -17,6 +27,10 @@ class App extends React.Component {
       </div>
     );
   }
+  onSelect(): void {
+    return;
+  }
+
   
 }
 export default App;
