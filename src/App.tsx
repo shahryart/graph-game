110c5407211ccf0f31a6c40c51201c0a32054678
Shahryar Tariq <st876@cornell.edu>
2018-01-12 16:51:09 +0500

    add more graphs

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,18 @@ import Dropdown from 'react-dropdown';
 const options = [ 'default-triangle'];
 
 class App extends React.Component {
+  timeSteps: number = 0;
+  state = {
+   
+    graph: undefined,
+    timeSteps: 1,
+
+  };
+  constructor(props: any) {
+    super(props);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
   render() {
     return (
       <div className="App">
@@ -16,21 +28,27 @@ class App extends React.Component {
             onChange={this.onSelect}
             placeholder="Select an option" 
           />
-          <input ref="input" defaultValue="1"/>
-          <button type="change" className="btn btn-primary">Change</button>
+          <input ref="input" defaultValue="1" onChange={this.handleTextChange}/>
+          <button type="change" className="btn btn-primary" onClick={this.handleClick}>Change</button>
         </div>
         <p className="Planar Graph Game">
         <Graph
-          timeSteps={1}
+          timeSteps={this.state.timeSteps}
         />
         </p>
       </div>
     );
   }
+  handleTextChange(e: any): void {
+    this.timeSteps = parseInt(e.target.value, 10);
+
+  }
+  handleClick(): void {
+    let i = this.timeSteps;
+    this.setState({graph: undefined, timeSteps: i});
+  }
   onSelect(): void {
     return;
   }
-
-  
 }
 export default App;
